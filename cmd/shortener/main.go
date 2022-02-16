9bc347cfbb169606f2e8579a9af761d6018fce8d
Anton <antonioo83@mail.ru>
2022-02-16 13:26:49 +0300

    Project: used "server" directory;

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/antonioo83/shot-url-service/internal/handlers"
+	"github.com/antonioo83/shot-url-service/internal/server"
 	"log"
 	http "net/http"
 )
 
 func main() {
 	// маршрутизация запросов обработчику
-	http.HandleFunc("/", handlers.UrlHandler)
+	http.HandleFunc("/", server.UrlHandler)
 	// запуск сервера с адресом localhost, порт 8080
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
