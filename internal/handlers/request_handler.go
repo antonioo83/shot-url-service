845862e4644c211dc1f24f8fedaa20c2d198dc75
Anton <antonioo83@mail.ru>
2022-02-18 11:58:48 +0300

    Increment3, request_handler.go: removed not used "GetQuery" method;

--- internal/handlers/request_handler.go
+++ internal/handlers/request_handler.go
@@ -1,22 +1,9 @@
 package handlers
 
 import (
-	"errors"
 	"io"
 	"net/http"
 )
-
-func GetQuery(name string, r *http.Request) (string, error) {
-	parameter := ""
-	if r.Method == http.MethodGet {
-		parameter = r.URL.Query().Get(name)
-		if parameter == "" {
-			return "", errors.New("The query '" + name + "' parameter is missing")
-		}
-	}
-
-	return parameter, nil
-}
 
 func GetBody(r *http.Request) (string, error) {
 	b, err := io.ReadAll(r.Body)
