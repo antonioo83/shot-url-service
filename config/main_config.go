e2c5bb15ab6038f4e2a0972782f84e73ed4054ac
antonioo83 <jurchenko@softbalance.ru>
2022-03-04 13:09:10 +0300

    Project, main_config.go: added flags;

--- config/main_config.go
+++ config/main_config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/caarlos0/env/v6"
+	"github.com/spf13/pflag"
 	"log"
 )
 
@@ -10,18 +11,34 @@ type Config struct {
 	BaseUrl         string `env:"BASE_URL"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH"`
 	IsUseFileStore  bool
+	isInitialized   bool
 }
+
+var cfg Config
 
 func GetConfig() Config {
 	const ServerAddress string = ":8080"
 	const BaseUrl string = ""
 	const FileStoragePath string = "C:\\Users\\jurchenko\\GolandProjects\\short-url-service\\shot-url-service\\data\\database.txt"
-	var cfg Config
+
+	if cfg.isInitialized == true {
+		return cfg
+	}
+
 	err := env.Parse(&cfg)
 	if err != nil {
 		log.Println(err)
 	}
 
+	pflag.StringVar(&cfg.ServerAddress, "a", cfg.ServerAddress, "The address of the local server")
+	pflag.Lookup("a").NoOptDefVal = cfg.ServerAddress
+
+	pflag.StringVar(&cfg.BaseUrl, "b", cfg.BaseUrl, "Base address of the result short url")
+	pflag.Lookup("b").NoOptDefVal = cfg.BaseUrl
+
+	pflag.StringVar(&cfg.FileStoragePath, "f", cfg.FileStoragePath, "Full filepath to the file storage")
+	pflag.Lookup("f").NoOptDefVal = cfg.FileStoragePath
+	pflag.Parse()
 	if cfg.ServerAddress == "" {
 		cfg.ServerAddress = ServerAddress
 	}
@@ -34,6 +51,8 @@ func GetConfig() Config {
 	if cfg.FileStoragePath == "" {
 		cfg.IsUseFileStore = false
 	}
+
+	cfg.isInitialized = true
 
 	return cfg
 }
--- go.mod
+++ go.mod
@@ -12,5 +12,6 @@ require (
 	github.com/caarlos0/env/v6 v6.9.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
