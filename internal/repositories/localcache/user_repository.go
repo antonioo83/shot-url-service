2c73649fb0ccca6ece5d0dd3bef9682df5597cb5
antonioo83 <jurchenko@softbalance.ru>
2022-03-21 17:06:19 +0300

    Model, User: added;

---
+++ internal/repositories/localcache/user_repository.go
@@ -0,0 +1,45 @@
+package localcache
+
+import (
+	"fmt"
+	"github.com/antonioo83/shot-url-service/internal/models"
+	"github.com/antonioo83/shot-url-service/internal/repositories/interfaces"
+)
+
+type memoryUserRepository struct {
+	buffer map[int]models.User
+}
+
+func NewMemoryUserRepository(m map[int]models.User) interfaces.UserRepository {
+	return &memoryUserRepository{m}
+}
+
+func (m *memoryUserRepository) Save(model models.User) error {
+	m.buffer[model.CODE] = model
+
+	return nil
+}
+
+func (m *memoryUserRepository) FindByCode(code int) (*models.User, error) {
+	model, ok := m.buffer[code]
+	if !ok {
+		return nil, fmt.Errorf("can't find model in buffer for the code: %d", code)
+	}
+
+	return &model, nil
+}
+
+func (m *memoryUserRepository) IsInDatabase(code int) (bool, error) {
+	_, ok := m.buffer[code]
+
+	return ok, nil
+}
+
+func (m *memoryUserRepository) GetLastModel() (*models.User, error) {
+	lastModel, ok := m.buffer[len(m.buffer)-1]
+	if !ok {
+		return nil, nil
+	}
+
+	return &lastModel, nil
+}
