94325802d746fbbfc6b02613130590399e04a45f
antonioo83 <jurchenko@softbalance.ru>
2022-03-24 14:01:03 +0300

    main: check dump;

--- internal/repositories/database/database_repository.go
+++ internal/repositories/database/database_repository.go
@@ -6,7 +6,6 @@ import (
 	"github.com/antonioo83/shot-url-service/internal/repositories/interfaces"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
-	"os"
 )
 
 type databaseRepository struct {
@@ -32,14 +31,6 @@ func (r databaseRepository) Disconnect(context context.Context, conn *pgx.Conn) 
 }
 
 func (r databaseRepository) RunDump(context context.Context, conn *pgxpool.Pool, filepath string) error {
-	sqlDump, err := os.ReadFile(filepath)
-	if err != nil {
-		return err
-	} else if string(sqlDump) == "" {
-		return fmt.Errorf("the file dump is empty")
-	}
 
-	_, err = conn.Exec(context, string(sqlDump))
-
-	return err
+	return nil
 }
