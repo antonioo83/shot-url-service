9092a73bca61b41547f48df801ed3f2f2c3b21bb
antonioo83 <jurchenko@softbalance.ru>
2022-04-01 17:11:00 +0300

    fixed return error;

--- internal/repositories/database/user_repository.go
+++ internal/repositories/database/user_repository.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"context"
-	"errors"
 	"github.com/antonioo83/shot-url-service/internal/models"
 	"github.com/antonioo83/shot-url-service/internal/repositories/interfaces"
 	"github.com/jackc/pgx/v4"
@@ -26,8 +25,8 @@ func (u userRepository) Save(model models.User) error {
 func (u userRepository) FindByCode(code int) (*models.User, error) {
 	var model models.User
 	err := u.connection.QueryRow(u.context, "SELECT code, uid FROM users WHERE code=$1", code).Scan(&model.Code, &model.UID)
-	if errors.Is(err, pgx.ErrNoRows) {
-		return nil, err
+	if err == pgx.ErrNoRows {
+		return &model, nil
 	} else if err != nil {
 		return nil, err
 	}
@@ -44,14 +43,10 @@ func (u userRepository) IsInDatabase(code int) (bool, error) {
 func (u userRepository) GetLastModel() (*models.User, error) {
 	model := models.User{}
 	err := u.connection.QueryRow(u.context, "SELECT code, uid FROM users ORDER BY code DESC").Scan(&model.Code, &model.UID)
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-
-			return &model, nil
-		}
+	if err == pgx.ErrNoRows {
 
 		return &model, err
 	}
 
-	return &model, nil
+	return &model, err
 }
